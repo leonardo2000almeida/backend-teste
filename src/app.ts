025f03c82db45a95c992a31eb05903fdb8caa572
leonardo2000almeida <leonardo.oliveira@aluno.riogrande.ifrs.edu.br>
2021-10-14 00:42:50 -0300

    feature: allow cors for everyone

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,14 @@ import express from "express";
 import morgan from "morgan";
 import botRouter from "./routes/botRouter";
 import messageRouter from "./routes/messagesRouter";
+import cors from "cors";
 
 // dotenv.config({ path: path.join(__dirname, "../.env") });
 
 const app = express();
 
 app.use(morgan("tiny"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(botRouter);
